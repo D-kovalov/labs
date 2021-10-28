a45211825738639d7266ea97f4ac7fb62cdf8777
Daniil <91754035+D-kovalov@users.noreply.github.com>
2021-10-28 19:54:51 +0300

    Add files via upload

--- lab2.py
+++ lab2.py
@@ -1,68 +1,71 @@
-class main:
-    Squar="square"
-    d = "diogonal"
-    l="lenth circle"
-    k=" "
-    s="square circle"
-class triangle(main):
-    a="a"
-    b="b"
-    c="c"
-    k="0.5"
-    sinalpha="sin_bet_ab"
-    def square(self):
-        print(self.Squar+"=" +self.k+"*"+self.a+"*"+self.b +"*" +self.sinalpha)
+def parse_string(s):
+ tag_list = []
+ l = []
+ f = False
+ r = []
 
-class isosceles_triangle(triangle):
-    b="a"
-    def square_i(self):
-        print(self.square())
+ for i in list(s):
+  if f:
+   l.append(i)
+   if i == ">":
+    r.append("".join(l))
+    l = []
+    f = False
+  else:
+   if i == "<":
+    l.append(i)
+    f = True
+ return r
 
-class right_triangle (triangle):
-            sinalpha = "1"
 
-            def square_r(self):
-                print(self.square())
-class equilateral_triangle(triangle):
-    b="a"
-    sinalpha = "1"
-    k="sin(60)"
-    def square_e(self):
-        print(self.square())
-class rectangle(main):
-    aa="a"
-    bb="b"
-    d="diogonal"
-    def diogonal(self):
-        print(self.d+"=sqrt("+self.aa+"*"+self.aa+"+"+self.bb+"*"+self.bb+ ")")
-class kvadrat(rectangle):
-    bb="a"
-    def diogonal_k(self):
-        print(self.diogonal())
-class circle(main):
-    r="r"
-    k="2Pi"
-class l_circle(circle):
-    def len(self):
-        print(self.l+"="+self.k+"*"+self.r)
-class s_circle(circle):
-    k="Pi"
-    def sq(self):
-        print(self.s + "=" + self.k + "*" + self.r+ "*" + self.r)
+def check_tags(tags):
+ for i in tags:
+  if len(i.split("<")) != 2 or len(i.split(">")) != 2:
+   print("Bad tag:",i)
 
-triangle = triangle()
-triangle.square()
-isosceles_triangle=isosceles_triangle()
-isosceles_triangle.square_i()
-right_triangle=right_triangle()
-right_triangle.square_r()
-equilateral_triangle=equilateral_triangle()
-equilateral_triangle.square_e()
-rectangle= rectangle()
-rectangle.diogonal()
-kvadrat= kvadrat()
-kvadrat.diogonal_k()
-l_circle=l_circle()
-l_circle.len()
-s_circle=s_circle()
-s_circle.sq()
+#получаем имя тега, без < > или </ >
+def tag_name(t):
+ if t.startswith("</"):
+  return t[2:-1]
+ else:
+  return t[1:-1]
+
+
+def check_syntax(tags):
+ r = [] #стек
+
+ for i in tags:
+  n = tag_name(i)
+  
+  #если тег закрывающий и ему есть соответсвующий в стеке, удаляем, если нету то ошибка синтаксиса
+  if i.startswith("</"):
+   if not n in r:
+    print("Bad syntax",i)
+    return
+   r.remove(n)
+
+  #если тег открывающий то заносим в стек
+  else:
+   r.append(n)
+
+ #проверяем стек, в нём не должно быть тегов
+ if len(r) != 0:
+  print("Bad closer tags for",r)
+ else:
+  print("Syntax good!")
+
+
+
+s = "<t1> <t2></t2></t1>"
+#s = "<t1> <t2></t2></t3>"
+#s = "<t1> <t2></t2>"
+
+#парсим только теги, пропускаем текст и тд
+tag = parse_string(s)
+
+#проверяем теги на наличие лишних символов
+check_tags(tag)
+
+#проверяем синтаксис
+check_syntax(tag)
+
